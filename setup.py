3555a331135f7e407c382df1b0acb8396e0bebca
LytaNicoll <lyta.nicoll@gmail.com>
2019-11-01 17:46:20 -0600

    adding settup.py and testing function

---
+++ setup.py
@@ -0,0 +1,12 @@
+import distutils
+from distutils.core import setup
+
+setup(
+    name='Utils',
+    version='0.0.0',
+    packages=[''],
+    url='https://github.com/LytaNicoll/Utils',
+    license='Apache Licence 2.0',
+    author='Lyta Nicoll',
+    description='Mine',
+)
